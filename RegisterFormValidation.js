500d59a4631716625c8fe7e01409f615455e8f45
Abhimanyu <Abhimanyu@jublfood.com>
2021-04-27 15:15:11 +0530

    removed extra files

--- RegisterFormValidation.js
+++ RegisterFormValidation.js
@@ -1,65 +1,37 @@
-const usernameEl = document.querySelector("#name");
+const nameEl = document.querySelector("#name");
 const emailEl = document.querySelector("#email");
 const passwordEl = document.querySelector("#password");
 const confirmPasswordEl = document.querySelector("#confirmpassword");
 
 const form = document.querySelector("#register");
+var radiosGender = document.getElementsByName("gender");
 
-var inputArr = [usernameEl, emailEl, passwordEl, confirmPasswordEl];
-
-usernameEl.addEventListener("input", function (event) {
-  if (isBlank(event.target.value)) {
-    showError(usernameEl, "name can not be left blank");
-  } else if (!isBetween(3, 8, event.target.value.length)) {
-    showError(
-      usernameEl,
-      "Username must be between ${min} and ${max} characters."
-    );
-  } else {
-    showSuccess(usernameEl);
-  }
-});
-
-usernameEl.addEventListener("blur", function (event) {
-  if (isBlank(event.target.value)) {
-    showError(usernameEl, "name can not be left blank");
-  } else if (!isBetween(3, 8, event.target.value.length)) {
-    showError(
-      usernameEl,
-      "Username must be between ${min} and ${max} characters."
-    );
-  } else {
-    showSuccess(usernameEl);
-  }
-});
-
-function checkName(name) {
-  const min = 3,
-    max = 25;
+function validateName() {
+  const name = nameEl.value.trim();
+  const min = 3;
+  const max = 25;
 
   if (isBlank(name)) {
-    showError(usernameEl, "name can not be left blank");
+    showError(nameEl, "name cannot be blank.");
   } else if (!isBetween(min, max, name.length)) {
-    showError(
-      usernameEl,
-      "Username must be between ${min} and ${max} characters."
-    );
+    showError(nameEl, `name must be between ${min} and ${max} characters.`);
   } else {
-    showSuccess(usernameEl);
+    showSuccess(nameEl);
     return true;
   }
 
   return false;
 }
 
-function checkEmail(email) {
-  const min = 3,
-    max = 25;
+function validateEmail() {
+  const email = emailEl.value.trim();
+
+  console.log("email = " + email);
 
   if (isBlank(email)) {
-    showError(emailEl, "email can not be left blank");
+    showError(emailEl, "email cannot be blank.");
   } else if (!isEmailValid(email)) {
-    showError(emailEl, "Email is not valid.");
+    showError(emailEl, "email is not valid.");
   } else {
     showSuccess(emailEl);
     return true;
@@ -68,9 +40,11 @@ function checkEmail(email) {
   return false;
 }
 
-function checkPassword(password) {
+function validatePassword() {
+  const password = passwordEl.value;
+
   if (isBlank(password)) {
-    showError(passwordEl, "password can not be left blank");
+    showError(passwordEl, "password cannot be blank.");
   } else if (!isPasswordString(password)) {
     showError(
       passwordEl,
@@ -84,17 +58,17 @@ function checkPassword(password) {
   return false;
 }
 
-function checkConfirmPassword(password, confirmPassword) {
-  const min = 3,
-    max = 25;
+function validateConfirmPassword() {
+  const password = passwordEl.value;
+  const confirmPassword = confirmPasswordEl.value;
+
+  console.log("pass = " + password);
+  console.log("cPass = " + confirmPassword);
 
   if (isBlank(password)) {
-    showError(confirmPasswordEl, "enter your password again");
+    showError(confirmPasswordEl, "confirm password cannot be blank.");
   } else if (password !== confirmPassword) {
-    showError(
-      confirmPasswordEl,
-      "password and confirm password does not match"
-    );
+    showError(confirmPasswordEl, "The password does not match");
   } else {
     showSuccess(confirmPasswordEl);
     return true;
@@ -102,6 +76,62 @@ function checkConfirmPassword(password, confirmPassword) {
 
   return false;
 }
+
+function isGenderSelected() {
+  if (
+    !(
+      radiosGender[0].checked ||
+      radiosGender[1].checked ||
+      radiosGender[2].checked
+    )
+  ) {
+    showError(radiosGender[0], "Please select gender.");
+    return false;
+  }
+  showSuccess(radiosGender[0]);
+  return true;
+}
+
+form.addEventListener("input", function (e) {
+  switch (e.target.id) {
+    case "name":
+      validateName();
+      break;
+    case "email":
+      validateEmail();
+      break;
+    case "password":
+      validatePassword();
+      break;
+    case "confirmpassword":
+      validateConfirmPassword();
+      break;
+  }
+});
+
+form.addEventListener("submit", function (e) {
+  // prevent the form from submitting
+  e.preventDefault();
+
+  // validate fields
+  let isNameValid = validateName(),
+    isEmailValid = validateEmail(),
+    isPasswordValid = validatePassword(),
+    isConfirmPasswordValid = validateConfirmPassword(),
+    isGenderSelectedB = isGenderSelected();
+
+  let isFormValid =
+    isNameValid &&
+    isEmailValid &&
+    isPasswordValid &&
+    isConfirmPasswordValid &&
+    isGenderSelectedB;
+
+  // submit to the server if the form is valid
+  if (isFormValid) {
+    console.log("form submitted to server");
+  }
+});
 
 function isBlank(value) {
   return value === "" ? true : false;
@@ -113,7 +143,11 @@ function isBetween(min, max, value) {
 
 function isEmailValid(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
+  var isEmailValid = re.test(email);
+
+  console.log("isEmailValid = " + isEmailValid);
+
+  return isEmailValid;
 }
 
 function isPasswordString(password) {
